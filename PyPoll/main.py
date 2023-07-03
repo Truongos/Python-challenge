15fbb7bde4ad294f2b48a090d1d9d7312e2a612d
Truongos <NickTruong@Knights.UCF.edu>
2023-07-02 22:04:17 -0400

    Okay

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,12 +3,15 @@ import csv
 import os
 
 # Files to load and output (Remember to change these)
-file_to_load = os.path.join("Resources", "election_data.csv")
+# file_to_load = os.path.join("Resources", "election_data.csv")
+file_to_load = "PyPoll\Resources\election_data.csv"
 
 total_votes = 0
 
-candiate_names = [] # empty list
+candidate_names = [] # empty list
+
 candidate_votes = {} # empty dictionary - key:value pair
+
 
 winning_candidate = ""
 winning_count = 0
@@ -23,12 +26,13 @@ with open(file_to_load) as election_data:
     for row in reader:
         total_votes = total_votes + 1
 
+       
+       
         candidate_name = row[2]
 
-        check if candidate_name is in the names list
-        if yes - add 1 to the candidate's vote count in the dictionary
-        if no - add the candidate_name to the names list and set the new candidate's vote count to 1
-
+        # check if candidate_name is in the names list
+        # if yes - add 1 to the candidate's vote count in the dictionary
+        # if no - add the candidate_name to the names list and set the new candidate's vote count to 1
 
 
 print(total_votes)
